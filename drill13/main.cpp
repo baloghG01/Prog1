cf129d043d1eaa8c63cd53f41a6b230055f80210
baloghG01 <busnes14@gmail.com>
2021-03-26 20:45:03 +0100

    13-as drill elkeszult

--- drill13/main.cpp
+++ drill13/main.cpp
@@ -1,54 +1,76 @@
-/*
-    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
-*/
 #include "Simple_window.h"
 #include "Graph.h"
 
+using namespace Graph_lib;
 
 int main()
 {
-    using namespace Graph_lib;
+	Point beginning {100,100};
+	int win_x=800, win_y=1000;
 
-    Point akatsuki {150,150};
-    Simple_window win1 {akatsuki,600,400,"my window"};
-    Line horizontal {akatsuki,Point{250,150}};
-    Line vertical {Point{200,100},Point{200,200}};
-    Closed_polyline neesan = {{120,320}, {170,210}, {300,250}, {300,300}, {130,270}};
-    neesan.set_fill_color(Color::yellow);
-    Open_polyline onichan = {{320,120}, {270,220}, {370,270}, {420,220}};
-    onichan.set_color(Color::red);
-    Rectangle shinigami{{300,100},100,160};
-    shinigami.set_fill_color(Color::black);
-    Text Madara{{100,300},"Watashi wa hogake desu"};
-    Madara.set_color(Color::dark_magenta);
-    Circle kira {{400,100},50};
-    kira.set_color(Color::magenta);
-    Ellipse yagami {{250,100},50,100};
-    //a marks-ok valamiert nem mukodnek
-    Image be {{0,0},"madara-san.jpeg"};
+	Simple_window win (beginning,win_x,win_y,"Christmas");
 
+	int grid_x=100;
+	int grid_y=100;
 
-    
+	Lines grid;
 
+	for(int x=grid_x;x<win_x;x+=grid_x)
+	{
+		grid.add(Point{x,0},Point(x,win_y-200));
+	}
 
+	for(int y=grid_y;y<win_y;y+=grid_y)
+	{
+		grid.add(Point{0,y},Point(win_x,y));
+	}	
 
-    
-    
-    win1.attach(be);
-    win1.attach(shinigami);
-    win1.attach(horizontal);
-    win1.attach(vertical);
-    win1.attach(neesan);
-    win1.attach(onichan);
-    win1.attach(Madara);
-    win1.attach(kira);
-    win1.attach(yagami);
+	win.attach(grid);
 
-    win1.wait_for_button();
+	Vector_ref<Graph_lib::Rectangle> reds;
 
+	for(int i=0;i<8;i++)
+	{
+		reds.push_back(new Rectangle {Point{i*100,i*100}, 100, 100});
+		reds[i].set_fill_color(Color::red);
+		win.attach(reds[i]);
+	}
 
+	Image h1 {Point{400,100}, "tms.jpeg"};
+	win.attach(h1);
+	Image h2 {Point{600,300}, "tms.jpeg"};
+	win.attach(h2);
+	Image h3 {Point{100,400}, "tms.jpeg"};
+	win.attach(h3);
+	Image h4 {Point{300,600}, "tms.jpeg"};
+	win.attach(h4);
 
+	Image jack {Point{0,0}, "jaks.jpeg"};
+	win.attach(jack);
 
-    
+	win.wait_for_button();
 
+	int rows_done=0;
+	while(true)
+	{
+		for(int i=0;i<7;i++)
+		{
+			jack.move(100,0);
+			win.wait_for_button();
+		}
+		if(rows_done==7)
+		{
+			rows_done=0;
+			jack.move(-700,-700);
+		}
+		else
+		{
+			jack.move(-700,100);
+			rows_done++;
+		}
+		win.wait_for_button();
+	}
+	
+
+	win.wait_for_button();
 }
